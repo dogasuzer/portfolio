1b7998bdb28038c5f1ddb23f281e519771bd548d
dogasuzer <111598762+dogasuzer@users.noreply.github.com>
2022-10-05 13:38:41 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+import ReactDOM from "react-dom" ;
+import Userform from "./components/Adduser"
+
+function App() {
+  return (
+    <div className="App">
+      <Userform/>
+    </div>
+  );
+}
+
+export default App;
